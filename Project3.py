1520363f0273ee7b2bf2287a241f90dcad953818
Jared Immerman <jimmerman819@gmail.com>
2024-05-13 22:35:26 -0700

    Fixed issues, able to run

--- Project3.py
+++ Project3.py
@@ -18,8 +18,8 @@ xytechCollection = db['Workorder/Location']
 parser = argparse.ArgumentParser(description ='SOMETHING')
 parser.add_argument('--baselight', type=argparse.FileType('r'))
 parser.add_argument('--xytech', type=argparse.FileType('r'))
-parser.add_argument('--process', dest='input')
-parser.add_argument('--output')
+parser.add_argument('--process', type=bool)
+# parser.add_argument('--output')
 
 args = parser.parse_args()
 
@@ -27,7 +27,6 @@ args = parser.parse_args()
 if args.xytech:
     with args.xytech as f:
         XY_File = f.read().splitlines()
-    xytechColl = xytechCollection.insert_many(XY_File)
 
 #read baselight
 if args.baselight:
@@ -43,76 +42,78 @@ csvFile.close()
 
 print(XY_File)
 
-for currentReadLine in BL_File:
-    
-    #print currentReadLine
-    print("\nCurrent read: " + currentReadLine)
+if args.process:
+    for currentReadLine in BL_File:
+        
+        #print currentReadLine
+        print("\nCurrent read: " + currentReadLine)
 
-    #parse currentReadLine
-    fixFrames = currentReadLine.split()
-    print("Parsed Line: ", fixFrames)
+        #parse currentReadLine
+        fixFrames = currentReadLine.split()
+        print("Parsed Line: ", fixFrames)
 
-    #pop file location from parseLine
-    currentFolder = fixFrames.pop(0)
-    #parse folder
-    parseFolder = currentFolder.split("/")
-    #pop system location from parseFolder
-    parseFolder.pop(1)
-    #merge parseFolder with file location layout
-    newFolder = "/".join(parseFolder)
+        #pop file location from parseLine
+        currentFolder = fixFrames.pop(0)
+        #parse folder
+        parseFolder = currentFolder.split("/")
+        #pop system location from parseFolder
+        parseFolder.pop(1)
+        #merge parseFolder with file location layout
+        newFolder = "/".join(parseFolder)
 
-    print("parseFolder: ", parseFolder)
-    print("newfolder: ", newFolder)
+        print("parseFolder: ", parseFolder)
+        print("newfolder: ", newFolder)
 
-    for techFile in XY_File:
-        #if folder from BL matches folder from Xytech, make currentFolder
-        if newFolder in techFile:
-            currentFolder = techFile.strip()
-            print("Current Folder: ", currentFolder)
+        for techFile in XY_File:
+            #if folder from BL matches folder from Xytech, make currentFolder
+            if newFolder in techFile:
+                currentFolder = techFile.strip()
+                print("Current Folder: ", currentFolder)
 
-    print("Fix Frames: " , fixFrames)
+        print("Fix Frames: " , fixFrames)
 
-    tempStart = 0
-    tempLast = 0
-    count = 0
-    for numb in fixFrames:
-        csvFile.close()
-        #if error found, pop from parseLine
-        if not numb.isnumeric():
-            continue
-        count += 1
-        #for numbers that are in a row
-        if tempStart == 0:
-            tempStart = numb
-            continue
-        #if numb is start + 1, set last = numb
-        if numb == str(int(tempStart) + 1) or numb == str(int(tempLast) + 1):
-            tempLast = numb
-            continue
-        #check if numb is greater than last + 1
-        elif int(numb) > (int(tempLast) + 1):
-            if int(tempLast) > 0:
-                print(currentFolder, tempStart + "-" + tempLast)
-                with open(csvFilename, "a") as csvFile:
-                    csvFile.write(f"{currentFolder},")
-                    csvFile.write(f" {tempStart}-{tempLast},\n")
-            else:
-                print(currentFolder, tempStart)
-                with open(csvFilename, "a") as csvFile:
-                    csvFile.write(f"{currentFolder},")
-                    csvFile.write(f" {tempStart}\n")
-            tempStart = numb
-            tempLast = 0
-    if int(tempLast) > 0:
-        print(currentFolder, tempStart + "-" + tempLast)
-        with open(csvFilename, "a") as csvFile:
-            csvFile.write(f"{currentFolder},")
-            csvFile.write(f" {tempStart}-{tempLast},\n")
-    else:
-        print(currentFolder, tempStart)
-        with open(csvFilename, "a") as csvFile:
-            csvFile.write(f"{currentFolder},")
-            csvFile.write(f" {tempStart},\n")
+        tempStart = 0
+        tempLast = 0
+        count = 0
+        for numb in fixFrames:
+            csvFile.close()
+            #if error found, pop from parseLine
+            if not numb.isnumeric():
+                continue
+            count += 1
+            #for numbers that are in a row
+            if tempStart == 0:
+                tempStart = numb
+                continue
+            #if numb is start + 1, set last = numb
+            if numb == str(int(tempStart) + 1) or numb == str(int(tempLast) + 1):
+                tempLast = numb
+                continue
+            #check if numb is greater than last + 1
+            elif int(numb) > (int(tempLast) + 1):
+                if int(tempLast) > 0:
+                    print(currentFolder, tempStart + "-" + tempLast)
+                    with open(csvFilename, "a") as csvFile:
+                        csvFile.write(f"{currentFolder},")
+                        csvFile.write(f" {tempStart}-{tempLast},\n")
+                else:
+                    print(currentFolder, tempStart)
+                    with open(csvFilename, "a") as csvFile:
+                        csvFile.write(f"{currentFolder},")
+                        csvFile.write(f" {tempStart}\n")
+                tempStart = numb
+                tempLast = 0
+        if int(tempLast) > 0:
+            print(currentFolder, tempStart + "-" + tempLast)
+            with open(csvFilename, "a") as csvFile:
+                csvFile.write(f"{currentFolder},")
+                csvFile.write(f" {tempStart}-{tempLast},\n")
+        else:
+            print(currentFolder, tempStart)
+            with open(csvFilename, "a") as csvFile:
+                csvFile.write(f"{currentFolder},")
+                csvFile.write(f" {tempStart},\n")
 
     csvFile.close
 
+#python3 Project3.py --baselight Baselight_export.txt --xytech Xytech.txt --process True
